976fbc78196cfb91ff31425b5ad3176b34f73124
Bakri Adi Himawan <bakri.himawan@sinarmasmining.com>
2024-03-12 13:23:44 +0700

    bugfix error params in controller employee

--- src/main/java/com/begroup/beartifact/controllers/EmployeeController.java
+++ src/main/java/com/begroup/beartifact/controllers/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.begroup.beartifact.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.time.LocalDate;
 
 import com.begroup.beartifact.models.Employee;
 import com.begroup.beartifact.services.EmployeeService;
@@ -29,19 +32,46 @@ public class EmployeeController {
         return employeeService.findEmployeeById(id);
     }
 
-    @PostMapping
-    public Employee createEmployee(@RequestBody Employee employee) {
+    // Content-Type application/json:
+    // @PostMapping
+    // public Employee createEmployee(@RequestBody Employee employee) {
+    //     return employeeService.saveEmployee(employee);
+    // }
+
+    // Content-Type multipart/form-data:
+    @PostMapping(consumes = "multipart/form-data")
+    public Employee createEmployee(
+        @RequestParam("name") String name,
+        @RequestParam("department") String department,
+        @RequestParam("email") String email,
+        @RequestParam("salary") Double salary,
+        @RequestParam("join_date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate joinDate
+    ) {
+
+        Employee employee = new Employee();
+
+        employee.setName(name);
+        employee.setDepartment(department);
+        employee.setEmail(email);
+        employee.setSalary(salary);
+        employee.setJoinDate(joinDate);
+        
         return employeeService.saveEmployee(employee);
     }
 
-    @PutMapping("/{id}")
-    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+    @PutMapping(value = "/{id}", consumes = "multipart/form-data")
+    public Employee updateEmployee(
+        @PathVariable Long id, 
+        @ModelAttribute Employee employeeDetails
+    ) {
         Employee employee = employeeService.findEmployeeById(id);
+
         employee.setName(employeeDetails.getName());
         employee.setDepartment(employeeDetails.getDepartment());
         employee.setEmail(employeeDetails.getEmail());
         employee.setSalary(employeeDetails.getSalary());
         employee.setJoinDate(employeeDetails.getJoinDate());
+
         return employeeService.saveEmployee(employee);
     }
 
